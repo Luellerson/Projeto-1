877a5d32d92c037f7f388c412236b3dedf6ac96d
Luellerson <ferreira.luellerson@gmail.com>
2023-11-14 18:23:03 -0300

    Realizada estilização com css3

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+    <title>Projeto 01</title>
+    <link rel="stylesheet" href="estilo/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="palavras-chave,do,meu,site">
     <meta name="description" content="Descrição do meu Website">
-    <title>Projeto 01</title>
 </head>
 <body>
     <header>
         <div class="center">
-            <div class="logo">Logomarca</div><!--logo-->
-            <nav class="desktop">
+            <div class="logo left">Logomarca</div><!--logo-->
+            <nav class="desktop right">
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -19,7 +22,10 @@
                     <li><a href="">Contato</a></li>
                 </ul>
             </nav>
-            <nav class="mobile">
+            <nav class="mobile right">
+                <div class="botao-menu-mobile">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -27,10 +33,13 @@
                     <li><a href="">Contato</a></li>
                 </ul>
             </nav>
+            <div class="clear"></div><!--center-->
         </div><!--center-->
+        
     </header>
 
     <section class="banner-principal">
+        <div class="overlay"></div><!--overlay-->
         <div class="center">
             <form action="">
                 <h2>Qual o seu melhor e-mail?</h2>
@@ -42,64 +51,69 @@
 
     <section class="descricao-autor">
         <div class="center">
-            <div class="w50">
+            <div class="w50 left">
                 <h2>Luellerson C. Ferreira</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id. Etiam vitae sodales eros. In in pretium elit. Nullam auctor ultrices porttitor. Curabitur hendrerit rhoncus vestibulum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id. Etiam vitae sodales eros. In in pretium elit. Nullam auctor ultrices porttitor. Curabitur hendrerit rhoncus vestibulum.</p>
             </div><!--w50-->
-            <div class="w50">
-                <img src="images/foto.png" alt="">
+            <div class="w50 left">
+                <img class="right" src="images/foto1.png" />
             </div><!--w50-->
+            <div class="clear"></div><!--center-->
         </div><!--center-->
     </section><!--descricao-autor-->
 
     <section class="especialidades">
-        <h2 class="title">Especialidades</h2>
         <div class="center">
-            <div class="box-especialidade">
-                <h3>icone1</h3>
-                <h3>CSS3</h3>
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+                <h4>CSS3</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
             </div><!--box-especialidade-->    
-            <div class="box-especialidade">
-                <h3>icone2</h3>
-                <h3>HTML5</h3>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+                <h4>HTML5</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
             </div><!--box-especialidade-->    
-            <div class="box-especialidade">
-                <h3>icone3</h3>
-                <h3>Javascript</h3>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+                <h4>Javascript</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
-            </div><!--box-especialidade-->    
+            </div><!--box-especialidade--> 
+            <div class="clear"></div><!--center-->   
         </div><!--center-->
     </section><!--especialidades-->
 
     <section class="extras">
         <div class="center">
-            <div class="w50">
-                <h2 class="title">Depoimentos</h2>
+            <div class="w50 left depoimentos-container">
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!--depoimento-single-->
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!--depoimento-single-->
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id."</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div><!--depoimento-single-->
             </div><!--w50-->
 
-            <div class="w50">
+            <div class="w50 left servicos-container">
                 <h2 class="title">Serviços</h2>
+                <div class="servicos">
                 <ul>
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
                 </ul>
+                </div><!--servicos-->
             </div><!--w50-->
+            <div class="clear"></div><!--center-->
         </div><!--center-->
     </section><!--extras-->
 
