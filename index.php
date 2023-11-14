9bcafa86d5d24fef24a37f9858b8d03c60c840f4
Luellerson <ferreira.luellerson@gmail.com>
2023-11-14 18:07:59 -0300

    Criação da estrutura HTML

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do meu Website">
+    <title>Projeto 01</title>
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div><!--center-->
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+            <form action="">
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div><!--center-->
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50">
+                <h2>Luellerson C. Ferreira</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id. Etiam vitae sodales eros. In in pretium elit. Nullam auctor ultrices porttitor. Curabitur hendrerit rhoncus vestibulum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id. Etiam vitae sodales eros. In in pretium elit. Nullam auctor ultrices porttitor. Curabitur hendrerit rhoncus vestibulum.</p>
+            </div><!--w50-->
+            <div class="w50">
+                <img src="images/foto.png" alt="">
+            </div><!--w50-->
+        </div><!--center-->
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+            <div class="box-especialidade">
+                <h3>icone1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
+            </div><!--box-especialidade-->    
+            <div class="box-especialidade">
+                <h3>icone2</h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
+            </div><!--box-especialidade-->    
+            <div class="box-especialidade">
+                <h3>icone3</h3>
+                <h3>Javascript</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</p>
+            </div><!--box-especialidade-->    
+        </div><!--center-->
+    </section><!--especialidades-->
+
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor. Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi. Donec tempus quam orci, ut consectetur metus venenatis id.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare blandit finibus. Sed elementum est dui, mollis sodales turpis porta hendrerit. Sed viverra ante nisl, sit amet ornare nisi hendrerit sit amet. Sed nec congue tortor.Cras bibendum mattis semper. Aliquam lectus neque, dapibus a arcu sit amet, pulvinar semper nisi.</li>
+                </ul>
+            </div><!--w50-->
+        </div><!--center-->
+    </section><!--extras-->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados!</p>
+        </div><!--center-->
+    </footer>
+</body>
+</html>
